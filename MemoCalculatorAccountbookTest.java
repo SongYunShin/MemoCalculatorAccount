9a909a0814e380cfef87faad8c7e45c9b1524d3b
SongYunShin <33708814+SongYunShin@users.noreply.github.com>
2017-12-23 23:55:39 +0900

    수정

--- MemoCalculatorAccountbookTest.java
+++ MemoCalculatorAccountbookTest.java
@@ -14,40 +14,32 @@ import software_√÷¡æ.MemoCalculatorAccountbook.CalculatorPanel;
 
 public class MemoCalculatorAccountbookTest {
 
-	private JFrame frm;
-	private JTabbedPane tabbedPane;
-	public CalculatorPanel Panel1;
-	MemoCalculatorAccountbook memcalacc;
-	Calculator cal;
-	/*@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}*/
+   private JFrame frm;
+   private JTabbedPane tabbedPane;
+   public CalculatorPanel Panel1;
+   
+   Calculator cal;
 
-	@Before
-	public void setUp() throws Exception {
-		memcalacc =  new MemoCalculatorAccountbook();
-		//cal = new Calculator(); 
-	}
+   @Test
+   public void testMemoCalculatorAccountbook(){
+      Calculator cal = new Calculator();
+      if(cal.symbol == 'c'&& cal.calNum == 50)
+            assertTrue(cal.result == 122);
+      if(cal.symbol == 'f'&& cal.calNum == 122)
+            assertTrue(cal.result == 50);
+      if(cal.symbol == 'i'&& cal.calNum == 1)
+            assertTrue(cal.result == 2.54);
+      
+      
+   }
 
-	@Test
-	public void testMemoCalculatorAccountbook(){
-		Calculator cal = new Calculator();
-		if(cal.symbol == 'c'&& cal.calNum == 50)
-		      assertTrue(cal.result == 122);
-		if(cal.symbol == 'f'&& cal.calNum == 122)
-		      assertTrue(cal.result == 50);
-		if(cal.symbol == 'i'&& cal.calNum == 1)
-		      assertTrue(cal.result == 2.54);
-		if(cal.symbol == '+'&& cal.calNum == 15 && cal.calNum == 10)
-		      assertTrue(cal.result == 25);
-		if(cal.symbol == '-'&& cal.calNum == 15 && cal.calNum == 10)
-		      assertTrue(cal.result == 5);
-		
-	}
-
-	/*@Test
-	public void testMain() {
-		fail("Not yet implemented");
-	}*/
+   @Test
+   public void testMain() {
+      Calculator cal = new Calculator();
+      if(cal.symbol == '+'&& cal.calNum == 15 && cal.calNum == 10)
+            assertTrue(cal.result == 25);
+      if(cal.symbol == '-'&& cal.calNum == 15 && cal.calNum == 10)
+            assertTrue(cal.result == 5);
+   }
 
 }
